229d7d47040e85f5f7e3b4a2924c1faca3a9743a
Yingquan Yu <42467994+OllieYu@users.noreply.github.com>
2019-09-11 16:15:35 +1200

    Add files via upload

---
+++ Version 0.2/TheTasteV0.0.1/ARViewController.swift
@@ -0,0 +1,176 @@
+//
+//  ViewController.swift
+//  ImageTrackV02
+//
+//  Created by LaoBoiii on 2019/8/18.
+//  Copyright © 2019年 theTaste. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ARViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    
+    struct videoContainer {
+        var targetName = String()
+        var videoPlayer = AVPlayer()
+        //var node : SCNNode?
+        var anchor : ARAnchor?
+        //var dead = false
+    }
+    
+    var videoContainers: Array<videoContainer> = Array()
+   // var anchorContainter : Array<ARAnchor> = Array()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARImageTrackingConfiguration()
+        if let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "menu", bundle: Bundle.main){
+            configuration.trackingImages = trackingImages
+            configuration.maximumNumberOfTrackedImages = 22
+        }
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        /*for vc in videoContainers {
+            vc.videoPlayer.pause()
+            //sceneView.session.remove(anchor: vc.anchor!)
+            
+            if let imageAnchor = vc.anchor as? ARImageAnchor{
+                sceneView.session.remove(anchor: imageAnchor)
+                print("removed")
+            }
+            
+        }
+        */
+        /*
+        for aa in anchorContainter /*sceneView.session.currentFrame!.anchors*/ {
+            sceneView.session.remove(anchor: aa)
+            print("AAA REMOVED")
+        }*/
+        
+        videoContainers.removeAll()
+        
+        //sceneView.scene.rootNode.enumerateChildNodes {
+        //    (node, stop) in node.removeFromParentNode()
+        //}
+        let configuration = ARImageTrackingConfiguration()
+    
+        sceneView.session.pause()
+        sceneView.scene.rootNode.enumerateChildNodes { (node, stop) in
+            node.removeFromParentNode()
+        }
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if let vaildAnchor = anchor as? ARImageAnchor{
+            //anchorContainter.append(vaildAnchor)
+            
+            /*for (index, vc) in videoContainers.enumerated(){
+                if(vc.targetName == vaildAnchor.referenceImage.name){
+                    //sceneView.session.remove(anchor: vaildAnchor)
+                    node.removeFromParentNode()
+                    videoContainers.remove(at: index)
+                }
+            }*/
+            
+            let videoPlayerNode = SCNNode()
+            var videoPlayer = AVPlayer()
+                
+            if let targetName = vaildAnchor.referenceImage.name, let validURL = Bundle.main.url(forResource: targetName, withExtension: "mp4", subdirectory: "/art.scnassets"){
+                videoPlayer = (AVPlayer(url: validURL))
+                videoPlayer.play()
+            }
+                
+            let videoPlayerGeometry = SCNPlane(width: vaildAnchor.referenceImage.physicalSize.width, height: vaildAnchor.referenceImage.physicalSize.height)
+                
+            videoPlayerGeometry.firstMaterial?.diffuse.contents = videoPlayer
+            videoPlayerNode.geometry = videoPlayerGeometry
+            videoPlayerNode.eulerAngles.x = -.pi / 2
+            node.addChildNode(videoPlayerNode)
+                
+            let vc = videoContainer(targetName: vaildAnchor.referenceImage.name ?? "error", videoPlayer: videoPlayer, anchor: anchor)
+            videoContainers.append(vc)
+
+
+        }
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        if let vaildAnchor = anchor as? ARImageAnchor{
+            
+           // if node.isHidden == true{
+           //     if let imageAnchor = anchor as? ARImageAnchor{
+           //         sceneView.session.remove(anchor: imageAnchor)
+           //     }
+           // }
+            
+            /*for (index, vc) in videoContainers.enumerated(){
+                if(vc.targetName == vaildAnchor.referenceImage.name){
+                    sceneView.session.remove(anchor: vaildAnchor)
+                    videoContainers.remove(at: index)
+                }
+            }*/
+            for vc in videoContainers{
+                if vc.targetName == vaildAnchor.referenceImage.name {
+                    
+                    if(node.isHidden) {
+                        vc.videoPlayer.pause()
+                    }
+                    else {
+                        vc.videoPlayer.play()
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    
+    /*func createdVideoPlayerNodeFor(_ target: ARReferenceImage) -> SCNNode{
+        
+        let videoPlayerNode = SCNNode()
+        
+        let videoPlayerGeometry = SCNPlane(width: target.physicalSize.width, height: target.physicalSize.height)
+        var videoPlayer = AVPlayer()
+        
+        if let targetName = target.name, let validURL = Bundle.main.url(forResource: targetName, withExtension: "mp4", subdirectory: "/art.scnassets"){
+            videoPlayer = AVPlayer(url: validURL)
+            videoPlayer.pause()
+        }
+        
+        
+        videoPlayerGeometry.firstMaterial?.diffuse.contents = videoPlayer
+        videoPlayerNode.geometry = videoPlayerGeometry
+        videoPlayerNode.eulerAngles.x = -.pi / 2
+        return videoPlayerNode
+    }*/
+    
+    /*override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+     guard let currentTouchLocation = touches.first?.location(in: self.sceneView), let hitTestResultNode = self.sceneView.hitTest(currentTouchLocation, options: nil).first?.node else { return }
+     if
+     
+     }*/
+    
+}
+
---
+++ Version 0.2/TheTasteV0.0.1/SocialViewController.swift
@@ -0,0 +1,111 @@
+//
+//  SocialViewController.swift
+//  TheTasteV0.0.1
+//
+//  Created by Danyal Cetin on 14/08/19.
+//  Copyright © 2019 ARTheTaste. All rights reserved.
+//
+
+import UIKit
+
+class SocialViewController: UIViewController {
+    
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    @IBOutlet weak var pageView: UIPageControl!
+    
+    let urlKeyArr = ["https://html.com/wp-content/uploads/flamingo.jpg",
+                     "https://html.com/wp-content/uploads/flamingo.jpg",
+                     "https://html.com/wp-content/uploads/flamingo.jpg",
+                     "https://html.com/wp-content/uploads/flamingo.jpg",
+                     "https://html.com/wp-content/uploads/flamingo.jpg",
+                     "https://html.com/wp-content/uploads/flamingo.jpg",
+                     "https://html.com/wp-content/uploads/flamingo.jpg",
+                     "https://html.com/wp-content/uploads/flamingo.jpg"]
+    
+    var imageArr = [UIImage]()
+    
+    var timer = Timer()
+    var counter = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pageView.numberOfPages = urlKeyArr.count
+        pageView.currentPage = 0
+        DispatchQueue.main.async {
+            self.timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(self.changeImage), userInfo: nil, repeats: true)
+        }
+        loadImages()
+    }
+    
+    func loadImages(){
+        for n in 0..<urlKeyArr.count{
+            if let url = URL(string: urlKeyArr[n]){
+                do{
+                    let data = try Data(contentsOf: url)
+                    imageArr.append(UIImage(data: data)!)
+                }catch let err{
+                    print("error : \(err.localizedDescription)")
+                    imageArr.append(UIImage(named: "error")!)
+                }
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @objc func changeImage() {
+        
+        if counter < urlKeyArr.count {
+            let index = IndexPath.init(item: counter, section: 0)
+            self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+            pageView.currentPage = counter
+            counter += 1
+        } else {
+            counter = 0
+            let index = IndexPath.init(item: counter, section: 0)
+            self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+            pageView.currentPage = counter
+            counter = 1
+        }
+    }
+}
+
+extension SocialViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return urlKeyArr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if let vc = cell.viewWithTag(111) as? UIImageView {
+
+            vc.image = imageArr[indexPath.row]
+
+        }
+        return cell
+    }
+}
+
+extension SocialViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = sliderCollectionView.frame.size
+        return CGSize(width: size.width, height: size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+}
+    
+
